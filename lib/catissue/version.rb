8f1479fd10f0915d3f8f2c61650fdc070f5a2595
Fred Loney <loneyf@ohsu.edu>
2011-03-04 18:06:00 -0800

    Specify caruby-core version dependency.

--- lib/catissue/version.rb
+++ lib/catissue/version.rb
@@ -1,6 +1,6 @@
 module CaTissue
   # The version of this caRuby Tissue release.
-  VERSION = "1.3.5"
+  VERSION = "1.3.6"
   
   # The supported caTissue release versions.
   CATISSUE_VERSIONS = "1.1.2"
