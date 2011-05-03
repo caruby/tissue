6d553f11efe955da9c6eb49603f01d4b24ab0fe6
Fred Loney <loneyf@ohsu.edu>
2011-05-03 16:24:12 -0700

    Move attributes to abstract class.

--- lib/catissue/domain/abstract_specimen_collection_group.rb
+++ lib/catissue/domain/abstract_specimen_collection_group.rb
@@ -2,7 +2,7 @@ module CaTissue
   resource_import Java::edu.wustl.catissuecore.domain.AbstractSpecimenCollectionGroup
 
   class AbstractSpecimenCollectionGroup
-    add_attribute_aliases(:diagnosis => :clinical_diagnosis)
+    add_attribute_aliases(:diagnosis => :clinical_diagnosis, :site => :specimen_collection_site, :collection_site => :specimen_collection_site)
 
     add_attribute_defaults(:activity_status => 'Active', :clinical_status => 'Not Specified', :clinical_diagnosis => 'Not Specified')
 
