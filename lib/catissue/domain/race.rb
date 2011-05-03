2834d3b6df3e0e53bceb7de894f15113a61022ba
Fred Loney <loneyf@ohsu.edu>
2011-05-03 16:28:14 -0700

    Add race name alias

--- lib/catissue/domain/race.rb
+++ lib/catissue/domain/race.rb
@@ -3,6 +3,8 @@ module CaTissue
   resource_import Java::edu.wustl.catissuecore.domain.Race
 
   class Race
+    add_attribute_aliases(:name => :race_name)
+    
     add_attribute_defaults(:race_name => 'Unknown')
   end
 end
