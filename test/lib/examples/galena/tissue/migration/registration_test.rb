5f00b5cfd70e276afeadbec1aa2141a847ba1af6
Fred Loney <loneyf@ohsu.edu>
2011-05-10 10:06:22 -0700

    Remove immaterial test_target block.

--- test/lib/examples/galena/tissue/migration/registration_test.rb
+++ test/lib/examples/galena/tissue/migration/registration_test.rb
@@ -5,15 +5,9 @@ module Galena
     class RegistrationTest < Test::Unit::TestCase
       include MigrationTestCase
     
-#      def test_target
-#        verify_target(:registration, :target => CaTissue::SpecimenCollectionGroup) do |scg|
-#          cpe = scg.collection_event
-#          assert_not_nil(cpe, "#{scg} missing CPE")
-#          pcl = cpe.protocol
-#          assert_not_nil(pcl, "#{scg} CPE #{cpe} missing protocol")
-#          assert_not_nil(pcl.short_title, "#{scg} protocol #{pcl} missing short title")
-#        end
-#      end
+      def test_target
+        verify_target(:registration, :target => CaTissue::SpecimenCollectionGroup)
+      end
     
       def test_save
         verify_save(:registration, :target => CaTissue::SpecimenCollectionGroup)
