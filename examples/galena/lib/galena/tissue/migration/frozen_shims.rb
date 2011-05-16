335eca1b373ea358e540721089c5144aaac08dd5
Fred Loney <loneyf@ohsu.edu>
2011-05-16 14:29:12 -0700

    Scope create_galena_box to class.

--- examples/galena/lib/galena/tissue/migration/frozen_shims.rb
+++ examples/galena/lib/galena/tissue/migration/frozen_shims.rb
@@ -11,7 +11,7 @@ module CaTissue
     # @param (see CaRuby::Migratable#migrate)
     def migrate(row, migrated)
       super
-      find or create_galena_box
+      find or StorageContainer.create_galena_box
     end
     
     private
@@ -20,13 +20,12 @@ module CaTissue
     # {Galena::Seed::Defaults#freezer_type}.
     # 
     # @return [StorageContainer] the new box
-    def create_galena_box
+    def self.create_galena_box
       defs = Galena::Seed.defaults
       self.storage_type = defs.box_type
       site = defs.tissue_bank
       # A freezer with a slot for the box.
       frz = defs.freezer_type.find_available(site, :create)
-      if frz.nil? then raise CaRuby::MigrationError.new("Freezer not available to place #{self}") end
       # Add this box to the first open slot in the first unfilled rack in the freezer.
       frz << self
     end
