6c0fae8cea45bb0006928e2cfe1eedd3f9293e2c
Fred Loney <loneyf@ohsu.edu>
2011-07-05 16:34:38 -0700

    Add proxy_for method.

--- lib/catissue/annotation/annotatable.rb
+++ lib/catissue/annotation/annotatable.rb
@@ -13,31 +13,22 @@ module CaTissue
         self.class.annotation_attribute?(attr) ? send(mth, *args) : super
       rescue AnnotationError
         raise
-      rescue Exception
+      rescue NoMethodError
         super
       end
     end
     
-    # Creates a {Annotation::Proxy} whose hook reference is set to this annotatable object.
-    #
-    # @param [Class] klass the proxy class
-    # @return [Resource] the new proxy
-    def create_proxy(klass)
-      # make the proxy instance
-      pxy = klass.new
-      # set the proxy hook reference to this hook instance
-      pxy.hook = self
-      logger.debug { "Generated #{qp} annotation proxy #{pxy} on demand." }
-      pxy
-    end
-    
-    # @param [AnnotationModule] mod the annotation module
-    # @return [Annotation] the hook -> proxy attribute
-    # @raise (see AnnotatableClass#annotation_proxy_attribute)
-    def annotation_proxy(mod)
-      pxy_attr = self.class.annotation_proxy_attribute(mod)
-      # the hook -> proxy attribute value
-      send(pxy_attr)
+    # @param [Symbol] attribute the hook => proxy attribute
+    # @param [Annotation] the annotation
+    # @return [Proxy] the hook proxy for the given annotation
+    def proxy_for(attribute, annotation)
+      @ann_pxy_hash ||= LazyHash.new do |ann|
+        pxy = self.class.attribute_metadata(attribute).type.new
+        pxy.hook = self
+        send(attribute) << pxy
+        pxy
+      end
+      @ann_pxy_hash[annotation]
     end
   end
 end
