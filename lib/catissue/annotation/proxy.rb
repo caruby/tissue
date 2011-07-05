2ce565050684bdbc1e463c0527fe4d90c93209f4
Fred Loney <loneyf@ohsu.edu>
2011-07-05 16:38:55 -0700

    identifier should not be ignored.

--- lib/catissue/annotation/proxy.rb
+++ lib/catissue/annotation/proxy.rb
@@ -2,14 +2,6 @@ module CaTissue
   module Annotation
     # {CaTissue::Resource} annotation hook proxy mix-in.
     module Proxy
-      # The hook proxy identifier is the hook identifier.
-      # This method delegates to the hook.
-      #
-      # @return [Integer] the hook identifier
-      def identifier
-        hook.identifier if hook
-      end
-      
       # @return [Annotatable] the annotated domain object  
       def hook
         send(self.class.owner_attribute_metadata.reader)
@@ -20,18 +12,10 @@ module CaTissue
         send(self.class.owner_attribute_metadata.writer, obj)
       end
       
-      # The hook proxy identifier cannot be set directly. Assignment is a no-op.
-      #
-      # @param [Integer] value the (ignored) identifier value
-      def identifier=(value); end
-      
-      # Sets the +id+ Java property to the hook identifier.
-      # This method must be called before saving an annotation that references this proxy.
-      def ensure_identifier_reflects_hook
-        if getId.nil? then
-          setId(hook.identifier)
-          logger.debug { "Set annotation proxy #{self} identifier to that of the hook entity #{hook.qp}." }
-        end
+      # Ensures that this proxy's hook exists in the database.
+      def ensure_hook_exists
+        if hook.nil? then raise AnnotationError.new("Annotation proxy #{self} is missing the hook domain object") end
+        hook.ensure_exists
       end
     end
   end
