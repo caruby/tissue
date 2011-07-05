5a5b6b3f251875b2963821d8d57d59ff6baaf804
Fred Loney <loneyf@ohsu.edu>
2011-07-05 16:41:02 -0700

    Add quirk tags.

--- lib/catissue/database/annotation/id_generator.rb
+++ lib/catissue/database/annotation/id_generator.rb
@@ -8,33 +8,33 @@ module CaTissue
         @executor = CaTissue::Database.instance.executor
       end
 
-      # caTissue alert - DE API subquery search fails.
-      # EntityManager.getNextIdentifierForEntity(EntityManager.java:2689) returns zero for some, but not all, DEs.
-      # This is a cascading error that is difficult to trace. The EntityManager.getNextIdentifierForEntity error
-      # is printed to the console rather than propagated up the call stack. A subsequent create then fails because
-      # the identifier is not set.
+      # @quirk caTissue DE API subquery search fails.
+      #   EntityManager.getNextIdentifierForEntity(EntityManager.java:2689) returns zero for some, but not all, DEs.
+      #   This is a cascading error that is difficult to trace. The EntityManager.getNextIdentifierForEntity error
+      #   is printed to the console rather than propagated up the call stack. A subsequent create then fails because
+      #   the identifier is not set.
       #
-      # A candidate work-around is to resolve the DE table name by issuing a direct SQL call and pass this
-      # to DE API EntityManagerUtil.getNextIdentifier. However, EntityManagerUtil is broken as well, for a
-      # different reason.
+      #   A candidate work-around is to resolve the DE table name by issuing a direct SQL call and pass this
+      #   to DE API EntityManagerUtil.getNextIdentifier. However, EntityManagerUtil is broken as well, for a
+      #   different reason.
       #
-      # Another candidate work-around is to get the next id manually from the caTissue DYEXTN_ID_GENERATOR table.
-      # However, this table is suspect since it is not defined as a database sequence generator and is not
-      # referenced in the caTissue source code. It is not used by the caTissue GUI when creating an annotation.
+      #   Another candidate work-around is to get the next id manually from the caTissue DYEXTN_ID_GENERATOR table.
+      #   However, this table is suspect since it is not defined as a database sequence generator and is not
+      #   referenced in the caTissue source code. It is not used by the caTissue GUI when creating an annotation.
       # 
-      # The work-around to the work-around to the work-around is to call the following SQL directly:
-      #   select max(identifier) from <table>
-      # where \<table\> is the result of the EntityManager work-around query. The EntityManager work-around query
-      # is described in
-      # https://cabig-kc.nci.nih.gov/Biospecimen/forums/viewtopic.php?f=19&t=404&p=2785&sid=febe0a1271b3d00020927741a94e9bff#p2785.
+      #   The work-around to the work-around to the work-around is to call the following SQL directly:
+      #     select max(identifier) from <table>
+      #   where \<table\> is the result of the EntityManager work-around query. The EntityManager work-around query
+      #   is described in
+      #   https://cabig-kc.nci.nih.gov/Biospecimen/forums/viewtopic.php?f=19&t=404&p=2785&sid=febe0a1271b3d00020927741a94e9bff#p2785.
       #
-      # Unfortunately, the +select max+ work-around is hampered by the obvious concurrent access race condition.
-      # For an unknown reason, caTissue DE does not use database sequence generators to make DE identifiers.
-      # Even if the caTissue DE API worked, it might suffer from the same race condition. The DE API
-      # EntityManagerUtil caches identifiers. The API call is synchronized, but it is unclear whether each DE
-      # client accesses the same EntityManagerUtil instance as the GUI DE action processor. If not, then the
-      # caTissue DE API, assuming that it were functional, would be open to an even more serious concurrency
-      # conflict than the work-around race condition.
+      #   Unfortunately, the +select max+ work-around is hampered by the obvious concurrent access race condition.
+      #   For an unknown reason, caTissue DE does not use database sequence generators to make DE identifiers.
+      #   Even if the caTissue DE API worked, it might suffer from the same race condition. The DE API
+      #   EntityManagerUtil caches identifiers. The API call is synchronized, but it is unclear whether each DE
+      #   client accesses the same EntityManagerUtil instance as the GUI DE action processor. If not, then the
+      #   caTissue DE API, assuming that it were functional, would be open to an even more serious concurrency
+      #   conflict than the work-around race condition.
       #
       # @param [String] the entity table
       # @return [Integer] a new identifier for the given annotation object
@@ -47,11 +47,11 @@ module CaTissue
         sql = TABLE_MAX_ID_SQL_TMPL % table
         result = @executor.execute { |dbh| dbh.select_one(sql) }
         max = result ? result[0].to_i : 0
-        identifier = max + 1
+        next_id = max + 1
         # End of work-around
         
-        logger.debug { "Next #{table} identifier is #{identifier}." }
-        identifier
+        logger.debug { "Next #{table} identifier is #{next_id}." }
+        next_id
       end
       
       private
