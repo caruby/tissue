a7d643d9ba56a35590483e443f1b98cea497ed11
Fred Loney <loneyf@ohsu.edu>
2011-07-05 16:30:01 -0700

    Add 1.2 SCG DE class changes.

---
+++ lib/catissue/domain/specimen_collection_group/pathology/base_pathology_annotation.rb
@@ -0,0 +1,16 @@
+module CaTissue
+  class SpecimenCollectionGroup
+    class Pathology
+      resource_import Java::pathology_scg.BasePathologyAnnotation
+      
+      class BasePathologyAnnotation
+        # @quirk caTissue The BasePathologyAnnotation => HistologicType collection
+        #   property is misnamed as histologicType rather than histologicTypeCollection. This misnaming
+        #   prevents caRuby from inferring the attribute domain type and inverse. Work-around is to set
+        #   these attribute features manually.
+        set_attribute_type(:histologic_type, CaTissue::SpecimenCollectionGroup::Pathology::HistologicType)
+        set_attribute_inverse(:histologic_type, :base_pathology_annotation)
+      end
+    end
+  end
+end
---
+++ lib/catissue/domain/specimen_collection_group/pathology/base_solid_tissue_pathology_annotation.rb
@@ -0,0 +1,16 @@
+module CaTissue
+  class SpecimenCollectionGroup
+    class Pathology
+      resource_import Java::pathology_scg.BaseSolidTissuePathologyAnnotation
+      
+      class BaseSolidTissuePathologyAnnotation
+        # @quirk caTissue The BaseSolidTissuePathologyAnnotation => HistologicGrade collection
+        #   property is misnamed as histologicGrade rather than histologicGradeCollection. This misnaming
+        #   prevents caRuby from inferring the attribute domain type and inverse. Work-around is to set
+        #   these attribute features manually.
+        set_attribute_type(:histologic_grade, CaTissue::SpecimenCollectionGroup::Pathology::HistologicGrade)
+        set_attribute_inverse(:histologic_grade, :base_solid_tissue_pathology_annotation)
+      end
+    end
+  end
+end
