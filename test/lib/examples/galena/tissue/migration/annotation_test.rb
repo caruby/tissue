1b3ee0e1948672f31daa0237553ea8ac104aa718
Fred Loney <loneyf@ohsu.edu>
2011-07-05 16:49:33 -0700

    Hook -> annotation proxy is 1:M association.

--- test/lib/examples/galena/tissue/migration/annotation_test.rb
+++ test/lib/examples/galena/tissue/migration/annotation_test.rb
@@ -7,14 +7,16 @@ module Galena
     
       def test_target
         verify_target(:annotation, :target => CaTissue::SpecimenCollectionGroup) do |scg|
-          pth = scg.pathology.radical_prostatectomy_pathology_annotations.first
-          assert_not_nil(pth, "Missing #{scg} annotation")
-          assert_not_nil(pth.comment, "Missing #{pth} comments")
-          gls = pth.gleason_score
-          assert_not_nil(pth, "Missing #{pth} gleason score")
+          pth = scg.pathology.first
+          assert_not_nil(pth, "Missing #{scg} pathology annotation proxy")
+          pst = pth.radical_prostatectomy_pathology_annotations.first
+          assert_not_nil(pst, "Missing #{scg} prostate annotation")
+          assert_not_nil(pst.comment, "Missing #{pst} comments")
+          gls = pst.gleason_score
+          assert_not_nil(pst, "Missing #{pst} gleason score")
           assert_equal('3', gls.primary_pattern_score, "Gleason score incorrect")
-          grd = pth.histologic_grades.first
-          assert_not_nil(grd, "Missing #{pth} grade")
+          grd = pst.histologic_grades.first
+          assert_not_nil(grd, "Missing #{pst} grade")
           assert_equal('2', grd.grade, "Grade incorrect")
         end
       end
