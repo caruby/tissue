1471c2b20eeae38af515a7a12dcff90d93d60d8f
Fred Loney <loneyf@ohsu.edu>
2011-07-05 16:46:24 -0700

    Add quirk tags.

--- lib/catissue/domain/specimen/pathology/specimen_additional_finding.rb
+++ lib/catissue/domain/specimen/pathology/specimen_additional_finding.rb
@@ -1,8 +1,11 @@
 module CaTissue
   class Specimen
+    # @quirk caTissue The 1.1 class AdditionalFinding is renamed to SpecimenAdditionalFinding in 1.2.
+    # Alias the Ruby class constant for forward and backaward compatibility.
+    #
+    # @quirk caTissue the Specimen additional finding annotation 1.1 method 'details' is renamed to
+    #   'specimenDetails' in 1.2.
     class Pathology
-      # caTissue alert - The 1.1 class AdditionalFinding is renamed to SpecimenAdditionalFinding in 1.2.
-      # Alias the Ruby class constant for forward and backaward compatibility.
       begin
         resource_import Java::pathology_specimen.SpecimenAdditionalFinding
         const_set(:AdditionalFinding, SpecimenAdditionalFinding)
@@ -10,8 +13,6 @@ module CaTissue
       rescue NameError
         logger.debug { "SpecimenAdditionalFinding pathology annotation class not found; attempting to import the caTissue 1.1 AdditionalFinding variant..." }
         resource_import Java::pathology_specimen.AdditionalFinding
-        # caTissue alert - the Specimen additional finding annotation 1.1 method 'details' is renamed to
-        # 'specimenDetails' in 1.2.
         const_set(:SpecimenAdditionalFinding, AdditionalFinding)
         logger.debug { "Aliased the caTissue 1.1 Specimen pathology annotation class AdditionalFinding class to the renamed 1.2 SpecimenAdditionalFinding." }
       end
