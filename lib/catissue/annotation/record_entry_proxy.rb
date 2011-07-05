efbcdd4f83c364f9111d9341ef56db9ea6329ac4
Fred Loney <loneyf@ohsu.edu>
2011-07-05 16:31:11 -0700

    1.2 proxy mechanism.

---
+++ lib/catissue/annotation/record_entry_proxy.rb
@@ -0,0 +1,20 @@
+require 'catissue/annotation/proxy'
+
+module CaTissue
+  module Annotation
+    # caTissue 1.2 {CaTissue::Resource} annotation hook proxy mix-in.
+    module RecordEntryProxy
+      include Proxy
+      
+      # @return [Annotatable] the annotated domain object  
+      def hook
+        send(self.class.owner_attribute_metadata.reader)
+      end
+      
+      # @param [Annotatable] obj the domain object to annotate  
+      def hook=(obj)
+        send(self.class.owner_attribute_metadata.writer, obj)
+      end
+    end
+  end
+end
