5403c4e1b1d87204a88db4076ddb3146f85b329c
Fred Loney <loneyf@ohsu.edu>
2011-08-03 13:31:16 -0700

    Add filters to examples.

--- examples/galena/lib/galena/tissue/migration/filter_shims.rb
+++ examples/galena/lib/galena/tissue/migration/filter_shims.rb
@@ -22,14 +22,6 @@ module CaTissue
   end
 
   class TissueSpecimen
-    # Transforms the +Frozen?+ flag input field to the caTissue specimen type +Frozen Tissue+ value.
-    #
-    # @param [String] value the input specimen type field
-    # @param @param [{Symbol => Object}] row the input row field => value hash
-    def migrate_specimen_type(value, row)
-      value =~ /TRUE/i ? 'Frozen Tissue' : 'Fixed Tissue'
-    end
-
     # Parses the source field as a UOM::Measurement if it is a string.
     # Otherwises, returns the source value.
     #
