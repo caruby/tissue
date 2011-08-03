285b00c72ede2a7d5fb73c5e4b4c49a59a426eac
Fred Loney <loneyf@ohsu.edu>
2011-08-03 14:53:31 -0700

    Clarify rubydoc.

--- lib/catissue/domain/order_details.rb
+++ lib/catissue/domain/order_details.rb
@@ -13,8 +13,11 @@ module CaTissue
       getOrderItemCollection or (self.order_items = Java::JavaUtil::LinkedHashSet.new)
     end
     
+    # @quirk caTissue Bug #64 - consent tier responses is not initialized to an empty set
+    #    in the Java constructor. Initialize it to a +LinkedHashSet+ in caRuby.
     def initialize
       super
+      # @quirk JRuby order_items property method isnot accessible until respond_to? is called.
       respond_to?(:order_items)
       self.order_items ||= Java::JavaUtil::LinkedHashSet.new
     end
