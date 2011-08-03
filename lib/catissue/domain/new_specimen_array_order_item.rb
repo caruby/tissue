41efe599fba9ffc627d5b57eab076ff4d6b9c135
Fred Loney <loneyf@ohsu.edu>
2011-08-03 14:53:22 -0700

    Clarify rubydoc.

--- lib/catissue/domain/new_specimen_array_order_item.rb
+++ lib/catissue/domain/new_specimen_array_order_item.rb
@@ -6,23 +6,24 @@ module CaTissue
 
   # The NewSpecimenArrayOrderItem domain class.
   class NewSpecimenArrayOrderItem
-    # @quirk caTissue Bug #64: Some domain collection properties not initialized.
-    #   Initialize order_items if necessary. 
+    # @quirk caTissue Bug #64: order items collection property is not initialized to an empty set
+    #    in the Java constructor. Initialize it to a +LinkedHashSet+ in caRuby. 
     #
     # @return [Java::JavaUtil::Set] the items
     def order_items
       getOrderItemCollection or (self.order_items = Java::JavaUtil::LinkedHashSet.new)
     end
     
-    # @quirk caTissue Bug #64: Some domain collection properties not initialized.
-    #   Initialize distributions if necessary. 
+   # @quirk caTissue Bug #64: distributions collection property is not initialized to an empty set
+    #    in the Java constructor. Initialize it to a +LinkedHashSet+ in caRuby.
     def distributions
       getDistributionCollection or (self.distributions = Java::JavaUtil::LinkedHashSet.new)
     end
 
     def initialize
       super
-      # jRuby bug? - Java methods not acceesible until respond_to? called; TODO - reconfirm this
+      # @quirk JRuby order_items and distributions property methods are not accessible until
+      # respond_to? is called.
       respond_to?(:order_items)
       respond_to?(:distributions)
       # work around caTissue Bug #64
