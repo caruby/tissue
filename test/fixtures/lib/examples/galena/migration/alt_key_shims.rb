33d90c7d2bc763e3f5d5d326565ebddddac5aa9c
Fred Loney <loneyf@ohsu.edu>
2011-10-24 17:18:07 -0700

    Move test base class and seed to helpers directory.

--- test/fixtures/lib/examples/galena/migration/alt_key_shims.rb
+++
@@ -1,7 +0,0 @@
-module CaTissue
-  shims Participant
-  
-  class Participant
-    set_secondary_key_attributes(:first_name, :last_name)
-  end
-end
--- test/lib/examples/galena/tissue/migration/participant_test.rb
+++ test/lib/examples/galena/tissue/migration/participant_test.rb
@@ -1,4 +1,4 @@
-require File.join(File.dirname(__FILE__), 'test_case')
+require File.dirname(__FILE__) + '/helpers/test_case'
 
 module Galena
   module Tissue
@@ -27,15 +27,17 @@ module Galena
         assert_nil(pnt.last_name, "Existing participant was updated despite the migration :create flag")
       end
       
+      # Tests creating a participant with the key set to the name rather the SSN.
       def test_create_with_alternate_id
         clear
-        # make the test participant
+        # Make the test participant.
         pnt = CaTissue::Participant.new(:first_name => 'Rufus', :last_name => 'Firefly')
-        # move aside duplicates
+        # Move aside duplicates.
         pnt.query.each do |dup|
           dup.last_name = dup.last_name.uniquify
           dup.save
         end
+        # Create the participant.
         pnt.create
         migrate_to_database(:participant, :target => CaTissue::Participant, :shims => [ALT_ID_SHIM], :create=>true)
         # Verify that the SSN was not updated.
@@ -46,7 +48,7 @@ module Galena
       
       private
       
-      ALT_ID_SHIM = 'test/fixtures/lib/examples/galena/migration/alt_key_shims.rb'
+      ALT_ID_SHIM = 'test/lib/examples/galena/migration/alt_key_shims.rb'
       
       def clear
         # push aside the existing test participant
