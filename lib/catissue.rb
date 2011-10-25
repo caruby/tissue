d4d776a80ebcfea5c5ff9567c193244bb1804ffc
Fred Loney <loneyf@ohsu.edu>
2011-10-24 17:32:05 -0700

    Move paths to domain. Top-level include is resource rather than domain.

--- lib/catissue.rb
+++ lib/catissue.rb
@@ -1,25 +1,4 @@
-# This file is the entry point included by applications which reference a CaTissue object or service.
+# This file is the entry point included by applications which use a CaTissue application service.
 
-# the caRuby core gem
-require 'rubygems'
-begin
-  gem 'caruby-core'
-rescue LoadError
-  # if the gem is not available, then try a local development source
-  $:.unshift File.join(File.dirname(__FILE__), '..', '..', 'caruby', 'lib')
-end
-
-require 'caruby/util/log'
-require 'catissue/domain'
+require 'catissue/resource'
 require 'catissue/database'
-
-# CaTissue wraps the caTissue Java API.
-# See the caRuby[http://http://caruby.rubyforge.org/] home page for more information.
-module CaTissue
-  # @param [<String>] nodes the path components relative to the caRuby Tissue source directory
-  # @return [String] the file path to the specified path components
-  def self.path(*nodes)
-    root = File.join(File.dirname(__FILE__), '..')
-    File.expand_path(File.join(*nodes), root)
-  end
-end
