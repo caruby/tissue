a550894aa7f6d24f12a273be295657a1600508bc
Fred Loney <loneyf@ohsu.edu>
2011-11-08 17:09:47 -0800

    Clean up.

--- lib/catissue/helpers/controlled_value.rb
+++ lib/catissue/helpers/controlled_value.rb
@@ -2,22 +2,23 @@ require 'caruby/helpers/controlled_value'
 
 module CaTissue
   class ControlledValue < CaRuby::ControlledValue
-
-    PUBLIC_ID_ALIAS_MAP = {:tissue_site => 'Tissue_Site_PID', :clinical_diagnosis => 'Clinical_Diagnosis_PID'}
-
-    # Returns the standard public id string for the given public_id_or_alias.
+    # @param [String, Symbol] public_id_or_alias the public id value or the
+    #   standard alias +:tissue_site+ or +:clinical_diagnosis+
+    # @return [String] the standard public id value for the given string or alias
     def self.standard_public_id(public_id_or_alias)
-      PUBLIC_ID_ALIAS_MAP[public_id_or_alias.to_sym] or public_id_or_alias.to_s
+      PID_ALIAS_HASH[public_id_or_alias.to_sym] or public_id_or_alias.to_s
     end
 
-    attr_accessor :identifier, :public_id
+    attr_accessor :identifier
 
-    attr_reader :identifier
+    attr_reader :public_id
 
+    # @return [Integer] the identifier of the parent
     def parent_identifier
       parent.identifier if parent
     end
 
+    # @param public_id_or_alias (see ControlledValue.standard_public_id)
     def public_id=(public_id_or_alias)
       @public_id = self.class.standard_public_id(public_id_or_alias)
     end
@@ -25,5 +26,10 @@ module CaTissue
     def to_s
       "#{value}"
     end
+    
+    private
+
+    # The public id symbol => name hash.
+    PID_ALIAS_HASH = {:tissue_site => 'Tissue_Site_PID', :clinical_diagnosis => 'Clinical_Diagnosis_PID'}
   end
 end
