d33cda98e90438dd2c08f1d0550bf1cbc765ee5e
Fred Loney <loneyf@ohsu.edu>
2011-11-08 18:55:33 -0800

    Cosmetic changes.

--- test/lib/catissue/domain/base_haemotology_pathology_test.rb
+++ test/lib/catissue/domain/base_haemotology_pathology_test.rb
@@ -6,19 +6,19 @@ class BaseHaematologyPathologyTest < Test::Unit::TestCase
   def setup
     super
     @scg = defaults.specimen_collection_group
-    @ann = CaTissue::SpecimenCollectionGroup::Pathology::BaseHaematologyPathologyAnnotation.new(:specimen_collection_group => @scg)
-    @hist = CaTissue::SpecimenCollectionGroup::Pathology::HistologicType.new(:base_pathology_annotation => @ann, :histologic_type => 'Adenocarcinoma - NOS')
-    @fnd = CaTissue::SpecimenCollectionGroup::Pathology::AdditionalFinding.new(:base_pathology_annotation => @ann, :pathologic_finding => 'Test finding')
+    @pth = CaTissue::SpecimenCollectionGroup::Pathology::BaseHaematologyPathologyAnnotation.new(:specimen_collection_group => @scg)
+    @hist = CaTissue::SpecimenCollectionGroup::Pathology::HistologicType.new(:base_pathology_annotation => @pth, :histologic_type => 'Adenocarcinoma - NOS')
+    @fnd = CaTissue::SpecimenCollectionGroup::Pathology::AdditionalFinding.new(:base_pathology_annotation => @pth, :pathologic_finding => 'Test finding')
     @dtl = CaTissue::SpecimenCollectionGroup::Pathology::Details.new(:additional_finding => @fnd, :detail => 'Test detail')
   end
 
   def test_dependents
-    assert_equal([@hist], @ann.histologic_types.to_a, "Annotation histologic types incorrect")
-    assert_equal([@fnd], @ann.additional_findings.to_a, "Annotation additional findings incorrect")
+    assert_equal([@hist], @pth.histologic_types.to_a, "Annotation histologic types incorrect")
+    assert_equal([@fnd], @pth.additional_findings.to_a, "Annotation additional findings incorrect")
     assert_equal([@dtl], @fnd.details.to_a, "Finding details incorrect")
   end
 
   def test_save
-    verify_save(@ann)
+    verify_save(@pth)
   end
 end
