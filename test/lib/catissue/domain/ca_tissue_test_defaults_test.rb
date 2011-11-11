bd9b3d1021213329540128f7b2509b7b081bbdc9
Fred Loney <loneyf@ohsu.edu>
2011-11-11 14:52:52 -0800

    No need to test find, since it is part of verify_save.

--- test/lib/catissue/domain/ca_tissue_test_defaults_test.rb
+++ test/lib/catissue/domain/ca_tissue_test_defaults_test.rb
@@ -3,16 +3,12 @@ require File.dirname(__FILE__) + '/../helpers/test_case'
 class CaTissueTestDefaultsTest < Test::Unit::TestCase
   include CaTissue::TestCase
 
+  # Validates the #{CaTissue::TestCase::Seed} data.
   def test_validation
     assert_nothing_raised(ValidationError, "Defaults validation unsuccessful") { defaults.add_defaults.validate }
   end
 
-  # Fetches the test data, creating new objects if necessary
-  def test_find
-    assert_nothing_raised("Defaults store unsuccessful") { defaults.domain_objects.each { |obj| database.find(obj) } }
-  end
-
-  # Store the test data
+  # Saves the #{CaTissue::TestCase::Seed} data.
   def test_save
     defaults.domain_objects.each { |obj| verify_save(obj) }
   end
