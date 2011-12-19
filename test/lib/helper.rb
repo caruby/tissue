fe3a26b9f2a56a4ec97161d2023e5563e1ce5c0e
Fred Loney <loneyf@ohsu.edu>
2011-12-19 10:53:57 -0800

    log is already required.

--- test/lib/helper.rb
+++ test/lib/helper.rb
@@ -3,7 +3,6 @@ require 'bundler/setup'
 Bundler.require(:test, :development)
 
 # Open the logger.
-require 'catissue/helpers/log'
 CaRuby::Log.instance.open(File.dirname(__FILE__) + '/../results/log/catissue.log',
   :shift_age => 5, :shift_size => 1048576, :debug => true)
 
