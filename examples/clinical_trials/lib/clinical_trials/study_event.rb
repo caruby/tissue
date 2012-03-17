e723cdef72faf3abf3f3a9a2f2916b1772c979c7
Fred Loney <loneyf@ohsu.edu>
2012-03-16 17:52:37 -0700

    Move Clinical Trials example to core.

--- examples/clinical_trials/lib/clinical_trials/study_event.rb
+++
@@ -1,8 +0,0 @@
-module ClinicalTrials
-  # Extends the StudyEvent domain class.
-  class StudyEvent
-    set_secondary_key_attributes(:study, :calendar_event_point)
-    
-    add_attribute_defaults(:calendar_event_point => 1.0)
-  end
-end
--- examples/clinical_trials/lib/clinical_trials/user.rb
+++
@@ -1,7 +0,0 @@
-module ClinicalTrials
-  # Import the Java class into Ruby.
-  # Extends the Study domain class.
-  class User
-    secondary_key_attributes = [:login]
-  end
-end
