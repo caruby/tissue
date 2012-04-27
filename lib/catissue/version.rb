9b20722cc680cbfedeadd63e7e5425ef8b323c67
Fred Loney <loneyf@ohsu.edu>
2012-04-26 17:02:47 -0700

    Bump version

--- lib/catissue/version.rb
+++ lib/catissue/version.rb
@@ -1,6 +1,6 @@
 module CaTissue
   # The version of this caRuby Tissue release.
-  VERSION = "2.1.3"
+  VERSION = "2.1.4"
   
   # The supported caTissue release versions.
   CATISSUE_VERSIONS = "1.1.2-1.2"
